ede2ef15bb83471f5fe3ca7b3dff7c1d37140f68
Kayro França <k4yro01@gmail.com>
2022-05-31 18:59:58 -0300

    add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import Search from '../search/Search';
+import './index.css';
+
+function Header() {
+    return (
+        <header className="headerContainer">
+            <Search />
+        </header>
+    );
+}
+
+export default Header;
