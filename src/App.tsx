709358c004797a79ab2ae0577e199e5491a4bd71
Kayro França <k4yro01@gmail.com>
2022-05-31 19:01:24 -0300

    update page

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/header/Header';
+import Map from './components/map/Map';
+
+function App() {
+
+  return (
+    <div className="mainContainer">
+      <Header />
+      <Map />
+    </div>
+  );
+}
+
+export default App;
