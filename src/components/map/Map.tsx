f08859e8130264448b919264de3e8ef251de2eca
Kayro França <k4yro01@gmail.com>
2022-06-02 13:02:20 -0300

    fixed position on map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -2,11 +2,17 @@ import { MapContainer, TileLayer } from "react-leaflet";
 import './index.css';
 
 function Map() {
-    const center = { lat: 51.721547, lng: -0.164687 };
-    const zoom = 8;
+    const center = { lat: -2.5606303, lng: -44.3281631 };
+    const zoom = 12;
 
     return (
-        <MapContainer center={center} zoom={zoom}>
+        <MapContainer
+            center={center} zoom={zoom}
+            dragging={false}
+            doubleClickZoom={false}
+            scrollWheelZoom={false}
+            attributionControl={false}
+            zoomControl={false}>
             <TileLayer
                 attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
